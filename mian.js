d9cfce2a1417b2ca69ddc58941d042fb9ed5f256
vizuone <agustin.lorenzon15@gmail.com>
2021-02-28 21:19:25 -0300

    primer commit

---
+++ mian.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click',() => {document.querySelector('.nav-menu').classList.toggle('show')}
+);
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.noticias',{delay: 500});
+ScrollReveal().reveal('.banner',{delay: 500});
+ScrollReveal().reveal('.banner-dos',{delay: 500});
+ScrollReveal().reveal('.social-media',{delay: 500});
+ScrollReveal().reveal('.links',{delay: 500});
+ScrollReveal().reveal('.footer-links',{delay: 500});
